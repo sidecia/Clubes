a02cbbc07d0d6b3b71f13f3f802bca2533959f1d
julio v <julio@imagencentral.com>
2015-02-16 16:45:42 -0600

    16/02/2015

---
+++ app/src/main/java/com/clubes/imagencentral/clubes/contenido.java
@@ -0,0 +1,45 @@
+package com.clubes.imagencentral.clubes;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class contenido extends Fragment {
+
+    /***/
+    // propiedades
+    public static final String ARG_SECTION_NAME = "section_name";
+    /***/
+
+
+    public contenido() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // inflar la vista
+        View vista = inflater.inflate(R.layout.fragment_contenido, container, false);
+
+        // atrapar los argumentos
+        Bundle argumentos = getArguments();
+
+        // poner el texto
+        TextView texto = (TextView) vista.findViewById(R.id.nombre_contenido);
+        texto.setText(argumentos.getString(ARG_SECTION_NAME));
+
+        return vista;
+
+    }
+
+
+}
