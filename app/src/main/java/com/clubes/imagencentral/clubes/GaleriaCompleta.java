607250bb756b6e815c970eac08cc06f071103c9e
julio v <julio@imagencentral.com>
2015-02-20 14:14:21 -0600

    clase para leer la API 17/02/2015

--- app/src/main/java/com/clubes/imagencentral/clubes/GaleriaCompleta.java
+++ app/src/main/java/com/clubes/imagencentral/clubes/GaleriaCompleta.java
@@ -3,12 +3,18 @@ package com.clubes.imagencentral.clubes;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+
+import com.clubes.imagencentral.clubes.adapters.JSONAdaptadorGaleriaPantallaCompleta;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 
 public class GaleriaCompleta extends FragmentActivity {
+
+    JSONArray items;
+    int posicion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +24,20 @@ public class GaleriaCompleta extends FragmentActivity {
         Intent intent=getIntent();
         try {
 
-            JSONArray items=new JSONArray(intent.getStringExtra("galeriaItems"));
-            int posicion=Integer.parseInt(intent.getStringExtra("posicion"));
+            items=new JSONArray(intent.getStringExtra("galeriaItems"));
+            posicion=Integer.parseInt(intent.getStringExtra("posicion"));
 
         } catch (JSONException e) {
         // TODO en caso de que falle el json
         }
 
         setContentView(R.layout.activity_galeria_pantalla_completa);
+
+        // encontrar el viewpager
+        ViewPager paginador=(ViewPager) findViewById(R.id.galeria_pantalla_completa);
+        JSONAdaptadorGaleriaPantallaCompleta adaptador=new JSONAdaptadorGaleriaPantallaCompleta(GaleriaCompleta.this, items);
+        paginador.setAdapter(adaptador);
+        paginador.setCurrentItem(posicion);
 
     }
 
---
+++ app/src/main/java/com/clubes/imagencentral/clubes/adapters/JSONAdaptadorGaleriaPantallaCompleta.java
@@ -0,0 +1,75 @@
+package com.clubes.imagencentral.clubes.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.clubes.imagencentral.clubes.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.clubes.imagencentral.clubes.tools.ImagenReal;
+
+/**
+ * Created by julio v on 20/02/2015.
+ */
+public class JSONAdaptadorGaleriaPantallaCompleta extends PagerAdapter {
+
+    private Context context;
+    private JSONArray items;
+    LayoutInflater inflater;
+
+
+    /***/
+    // instanciar UIL
+    private ImageLoader imageLoader=ImageLoader.getInstance();
+    DisplayImageOptions opciones=new DisplayImageOptions.Builder().build();
+    /***/
+
+
+    public JSONAdaptadorGaleriaPantallaCompleta(Context context, JSONArray items) {
+        this.context=context;
+        this.items=items;
+    }
+
+    public int getCount() {
+        return items.length();
+    }
+
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        // atrapar la vista
+        ImageView imagenItemGaleriaPantallaCompleta;
+        inflater=(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView=inflater.inflate(R.layout.item_galeria_pantalla_completa, container, false);
+        imagenItemGaleriaPantallaCompleta=(ImageView) itemView.findViewById(R.id.imagen_item_galeria_pantalla_completa);
+
+        // poner la imagen en la vista
+        try {
+
+            String imagenReal = new ImagenReal().cambiaImagen(items.getJSONObject(position).getString("photo"), "normal");
+            imageLoader.displayImage("http://192.168.0.103/mobile/club/recursos/img/"+imagenReal, imagenItemGaleriaPantallaCompleta, opciones);
+
+            ((ViewPager) container).addView(itemView);
+
+        } catch(JSONException e) {
+            // TODO en caso de que falle el json
+            e.printStackTrace();
+        }
+
+        return itemView;
+
+    }
+
+    public boolean isViewFromObject(View view, Object object) {
+        return view==object;
+    }
+
+}
